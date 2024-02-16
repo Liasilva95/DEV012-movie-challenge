1420220ad3dbdef7f01b6a07de349fc4cba56b75
liasilva95 <amaosilvalizbeth.1303@gmail.com>
2024-02-16 12:16:23 -0500

    estructura de html organizada, con header, h1, main, etc. estilos aplicados en css para todas las clases de html. lista de acciones en api.js y main.js

---
+++ src/api.js
@@ -0,0 +1,14 @@
+
+// Definir funcion searchMoviesByTitle (arg = pelicula)
+function searchMoviesByTitle(title) {
+    try {
+        const apiKey = "ee33331b"; // clave API de OMDb
+    }
+}
+// Hacer solicitud a la API de OMDb con fetch
+// Si la respuesta es exitosa (Response === "True"), se muestra informacion sobre la pelicula
+// 
+
+export default searchMoviesByTitle;
+
+// Peticiones
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import searchMoviesByTitle from './src/api.js';
+
+searchMoviesByTitle
+
+// idendentificar el titulo y el año 1
+
+// variables (titulo a buscar) input
+// variables
+// buscar boton. evento.2
+// obtener el valor del titulo o del año
+// ejecucion de la funcion de API -- devuelve arreglo de pelicula
+// validar que el arreglo sea correcto-- no estar vacio
+// encontrar peliculas-- si el paso es correcto.
