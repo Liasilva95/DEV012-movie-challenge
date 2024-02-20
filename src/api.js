6f739bd2a5349e2f29b0de522d6ec9a8c77f9a4b
liasilva95 <amaosilvalizbeth.1303@gmail.com>
2024-02-20 01:45:40 -0500

    Se logro hacer dos funciones en api.js donde se realiza la busqueda con fetch a traves del titulo y a traves del id. al ejecutarse responde como un arreglo la funcion searchMoviesByTitle y como objeto la funcion searchMoviesById. en los eventos se trabajo: 1 Identificar el titulo y el ID, 2 Ejecutar las dos funciones de busqueda, 3 Cual de las 2 funciones me devuelve un arreglo.

--- src/api.js
+++ src/api.js
@@ -1,14 +1,56 @@
+// Peticiones
 
+// FUNCTION BY TITLE
 // Definir funcion searchMoviesByTitle (arg = pelicula)
 function searchMoviesByTitle(title) {
-    try {
-        const apiKey = "ee33331b"; // clave API de OMDb
-    }
+  const apiKey = "ee33331b"; // clave API de OMDb
+  const apiUrl = `http://www.omdbapi.com/?s=${title}&apikey=${apiKey}`; //Interpolacion.
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.Response === "True") {
+        // Itera a través de los titulos y muestra los detalles de c/película
+        data.Search.forEach((movie) => {
+          console.log("Titulo:", movie.Title);
+          console.log("Año:", movie.Year);
+          console.log("Tipo:", movie.Type);
+          console.log("ID:", movie.imdbID);
+          console.log("Poster:", movie.Poster);
+        });
+      } else {
+        console.log("No se encontró información para la película:", title);
+      }
+    })
+    .catch((error) => {
+      console.log("Error al obtener datos de la API:", error);
+    });
 }
+
+// FUNCTION BY YEAR
+function searchMoviesById(ID) {
+  const apiKey = "ee33331b"; // Clave API de OMDb
+//protocolo(http-https), nombre del dominio, path name(opcional), query params(¿) nombre del parametro=valor.
+  const apiUrl = `http://www.omdbapi.com/?i=${ID}&apikey=${apiKey}`; 
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.Response === "True") {
+        console.log("Título:", data.Title);
+        console.log("Año:", data.Year);
+        console.log("Trama:", data.Plot);
+        console.log("Tipo:", data.Type);
+        console.log("ID:", data.imdbID);
+        console.log("Poster:", data.Poster);
+      } else {
+        console.log("No se encontró información para el id:", ID);
+      }
+    })
+    .catch((error) => {
+      console.error("Error al obtener datos de la API:", error);
+    });
+}
+
 // Hacer solicitud a la API de OMDb con fetch
-// Si la respuesta es exitosa (Response === "True"), se muestra informacion sobre la pelicula
-// 
+// Si la rpta es exitosa (Response === "True"), se muestra informacion sobre la pelicula
 
-export default searchMoviesByTitle;
-
-// Peticiones
+export { searchMoviesByTitle, searchMoviesById };
--- src/main.js
+++ src/main.js
@@ -1,13 +1,36 @@
-import searchMoviesByTitle from './src/api.js';
+import {searchMoviesByTitle, searchMoviesById} from './api.js';
 
-searchMoviesByTitle
 
-// idendentificar el titulo y el año 1
+const title = document.querySelector(".searching");
+const button = document.querySelector("#doondooneButton");
 
+//searchMoviesByTitle("Inception");
+//searchMoviesById("tt3896198");
+
+title.addEventListener("change", function(event) {
+    const inputValue = event.target.value;
+    console.log("El valor introducido es:", inputValue);
+
+    // Llama a la función de búsqueda por título
+    const searchResultByTitle = searchMoviesByTitle("Inception");
+
+    // Llama a la función de búsqueda por ID 
+    const movieId = "tt3896198";
+    const searchResultById = searchMoviesById(movieId);
+
+        const data = [searchResultByTitle, searchResultById];
+     // Verifica si la respuesta es un arreglo
+     const isArrayWithArrays = data.map(item => Array.isArray(item));
+
+    if (isArrayWithArrays[0] === true) {
+        console.log("La respuesta es un arreglo.");
+    } else {
+        console.log("La respuesta no es un arreglo.");
+    }
+});
+// identificar el titulo y el id
 // variables (titulo a buscar) input
-// variables
-// buscar boton. evento.2
-// obtener el valor del titulo o del año
-// ejecucion de la funcion de API -- devuelve arreglo de pelicula
-// validar que el arreglo sea correcto-- no estar vacio
-// encontrar peliculas-- si el paso es correcto.
+// ejecutar mis dos funciones de busqueda. 
+// validar cual de las 2 funciones me devuelve un arreglo.
+// validar que el objeto sea correcto-- no estar vacio
+// mostrar peliculas-- si el paso es correcto.
