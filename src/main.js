679cbf38c17e9e9b06f708b7f06bf5fa09980479
liasilva95 <amaosilvalizbeth.1303@gmail.com>
2024-02-24 03:41:20 -0500

    se logra mostrar las peliculas a traves del ingreso del titulo, y se trabajo en las propiedades css para la interfaz del usuario

--- src/main.js
+++ src/main.js
@@ -62,3 +62,44 @@ const button = document.querySelector("#doondooneButton");
       console.error("Error al buscar películas por ID:", error);
     });
 });
+
+const searchResultsElement = document.getElementById("search-results");
+
+title.addEventListener("change", function(event) {
+  const inputValue = event.target.value;
+  console.log("El valor introducido es:", inputValue);
+
+  searchMoviesByTitle(inputValue)
+    .then((searchResults) => {
+      // Limpia los resultados anteriores antes de mostrar los nuevos
+      searchResultsElement.innerHTML = "";
+
+      searchResults.forEach((movie) => {
+        const movieElement = document.createElement("div");
+        movieElement.innerHTML = `
+          <h2>${movie.Title}</h2>
+          <p>${movie.Year}</p>
+          <img src="${movie.Poster}" alt="${movie.Title} Poster">
+        `;
+        searchResultsElement.appendChild(movieElement);
+      });
+    })
+    .catch((error) => {
+      console.error("Error al buscar películas por título:", error);
+    });
+
+    const movieId = "tt1287845"; // Reemplaza con el ID de la película deseada
+
+searchMoviesById(movieId)
+  .then((movieDetails) => {
+    document.getElementById("movie-title").textContent = movieDetails.Title;
+    document.getElementById("movie-plot").textContent = movieDetails.Plot;
+    document.getElementById("movie-poster").src = movieDetails.Poster;
+    document.getElementById("movie-trama").src = movieDetails.Plot;
+  })
+  .catch((error) => {
+    console.error("Error al buscar película por ID:", error);
+  });
+
+});
+
